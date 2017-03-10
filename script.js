bb9f4f554a22c7192e183ca7a183461abe01a910
Jahmyl Connell <jahmylc@gmail.com>
2017-03-10 14:42:20 +0000

    script

---
+++ script.js
@@ -0,0 +1,17 @@
+TinyTurtle.apply(window); 
+
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(90);
+forward(50);
+right(50);
+forward(40);
+right(60);
+right(60);
+stamp();
+
